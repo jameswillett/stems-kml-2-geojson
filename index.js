58ed6c8b347a7a2c517e6fe0cda15f23867a76f9
james willett <jms.wlltt@gmail.com>
2017-10-01 00:42:53 -0400

    got rid of all those gosh darn returns

--- index.js
+++ index.js
@@ -7,22 +7,16 @@ const tj = require('togeojson');
 const files = finder.in(`${__dirname}/kml`).findFiles();
 
 //initialize + filters array to only include *.kml files
-const jsonArray = files.filter(file => {
-  return /.*\.kml/.test(file)
-})
+const jsonArray = files.filter(file => /.*\.kml/.test(file))
 
 //where the magic happens. kml to geojson here and we only care about the features property
-.map(file => {
-  return tj.kml(new DOMParser().parseFromString(fs.readFileSync(file,'utf8'))).features
-})
+.map(file => tj.kml(new DOMParser().parseFromString(fs.readFileSync(file,'utf8'))).features)
 
 //flattens array (because some cities have multiple features)
 .reduce((acc, cur) => acc.concat(cur), [])
 
 //removes 'point' features because theyre stupid
-.filter(file => {
-  return file.geometry.type != 'Point'
-})
+.filter(file => file.geometry.type != 'Point')
 
 //writes itttttttttt
 fs.writeFile('master.geojson', JSON.stringify(jsonArray), (err) => {
