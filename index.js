89e8be88956bb135612e7718129261c7a1fdb844
James Willett <jms.wlltt@gmail.com>
2018-12-15 21:48:00 -0500

    ramda overload

--- index.js
+++ index.js
@@ -7,33 +7,45 @@ const tj = require('togeojson');
 //grabs ALL files in ./kml
 const files = finder.in(`${__dirname}/kml`).findFiles();
 
-//initialize + filters array to only include *.kml files
-const jsonArray = files.filter(file => /.*\.kml/.test(file))
+const stringifiedGeoJSON = R.pipe(
+
+  // only grab kml files
+  R.filter(R.test(/.*\.kml/)),
 
   //where the magic happens. kml to geojson here and we only care about the features property
-  .map(file => tj.kml(new DOMParser().parseFromString(fs.readFileSync(file,'utf8'))).features)
+  R.map(
+    R.pipe(
+      f => tj.kml(new DOMParser().parseFromString(fs.readFileSync(f, 'utf8'))),
+      R.prop('features'),
+    )
+  ),
 
   //flattens array (because some cities have multiple features)
-  .reduce(R.concat, [])
+  R.flatten,
 
   //removes 'point' features because theyre stupid
-
-  .filter(
+  R.filter(
     R.pathSatisfies(
       R.complement(R.equals('Point')),
       ['geometry', 'type'],
     )
-  )
+  ),
 
-  //this is just to remove altitude. dont use for onfleet overlays.
-
-  // .map(R.over(R.lensPath(['geometry','coordinates']), R.map(R.map(R.take(2)))))
+  //this is just to remove altitude.
+  R.map(R.over(R.lensPath(['geometry','coordinates']), R.map(R.map(R.take(2))))),
 
   // remove properties object. maybe change this if we want to keep styling later
+  R.map(R.assoc("properties", {})),
 
-  .map(R.assoc("properties", {}));
+  // nest into object
+  R.assoc('features', R.__, { type: 'FeatureCollection' }),
+
+  // strigify that shit
+  JSON.stringify,
+)(files)
+
 
 //writes itttttttttt
-fs.writeFile('master.geojson', `{"type":"FeatureCollection","features":${JSON.stringify(jsonArray)}}`, (err) => {
+fs.writeFile('master.geojson', stringifiedGeoJSON, (err) => {
   if (err) console.log(err)
 })
